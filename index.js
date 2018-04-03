46ad6d16e61e8ff26ad392510a0c2309f791a750
Bryce Hull <bahull@live.com>
2018-04-03 10:24:20 -0500

    Perfect Square

--- index.js
+++ index.js
@@ -26,3 +26,21 @@ const descendingOrder = n =>
       .sort((a, b) => b - a)
       .join("")
   );
+
+//   You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+// Examples:
+
+// findNextSquare(121) --> returns 144
+// findNextSquare(625) --> returns 676
+// findNextSquare(114) --> returns -1 since 114 is not a perfect
+
+function findNextSquare(sq) {
+  let wholeNum = Math.sqrt(sq);
+  if (wholeNum % 1 !== 0) return -1;
+  return (wholeNum += 1) * wholeNum;
+}
