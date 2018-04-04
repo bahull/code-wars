785330f70cb9e4ef506ef34839aea5992e57558f
Bryce Hull <bahull@live.com>
2018-04-04 15:22:28 -0500

    Updated with new kata

--- index.js
+++ index.js
@@ -44,3 +44,14 @@ function findNextSquare(sq) {
   if (wholeNum % 1 !== 0) return -1;
   return (wholeNum += 1) * wholeNum;
 }
+//Sort Numbers
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+const solution = nums =>
+  nums !== null && nums !== undefined && nums.length > 0
+    ? nums.sort((a, b) => a - b)
+    : [];
