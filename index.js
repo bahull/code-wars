40ddbfbe69544384af40603d6ceb79713524c62a
Bryce Hull <bahull@live.com>
2018-04-06 10:27:19 -0500

    Kata for the day

--- index.js
+++ index.js
@@ -80,3 +80,13 @@ const countPositivesSumNegatives = input => {
   let negatives = input.reduce((sum, curr) => (curr <= 0 ? sum + curr : 0), 0);
   return [positives, negatives];
 };
+
+// Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+// Expected results:
+
+// spam(1);  // hue
+// spam(6);  // huehuehuehuehuehue
+// spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+
+const spam = num => "hue".repeat(num);
