12e990aa57822b3934e00f58ae518d403a655dd0
Bryce Hull <bahull@live.com>
2018-04-16 09:18:38 -0500

    Updated with new problem

--- index.js
+++ index.js
@@ -387,3 +387,40 @@ const checkThreeAndTwo = arr => {
   }
   return countArray.includes(2) && countArray.includes(3);
 };
+
+//Student's final grade
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// Examples:
+
+// finalGrade(100, 12);  // 100
+// finalGrade(99, 0);    // 100
+// finalGrade(10, 15);   // 100
+
+// finalGrade(85, 5);    // 90
+
+// finalGrade(55, 3);    // 75
+
+// finalGrade(55, 0);    // 0
+// finalGrade(20, 2);    // 0
+// *Use Comparison and Logical Operators.
+
+const finalGrade = (grade, projs) => {
+  if (grade > 90 || projs > 10) {
+    return 100;
+  } else if (grade > 75 && projs >= 5) {
+    return 90;
+  } else if (grade > 50 && projs >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+};
