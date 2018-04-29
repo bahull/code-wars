13c103e53de8897e38829b974c32033e46ba8223
Bryce Hull <bahull@live.com>
2018-04-29 16:31:34 -0500

    Updated previous title

--- index.js
+++ index.js
@@ -648,5 +648,6 @@ const stringSplitter = str => {
 
 stringSplitter(testString);
 
+// Even or Odd
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 const even_or_odd = num => (num % 2 === 0 ? "Even" : "Odd");
