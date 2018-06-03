f997285d9868dedec0c957497ec5122b1af109f0
Bryce Hull <bahull@live.com>
2018-06-03 18:13:17 -0500

    Updated with new kata

--- index.js
+++ index.js
@@ -811,3 +811,13 @@ function formatDuration(time) {
 }
 
 formatDuration(316);
+
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+// Valid passwords will only be alphanumeric characters.
+
+const validate = password => /(^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[.; ])(?=.{6,}))/.test(password);
