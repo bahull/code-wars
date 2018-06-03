67fb4729c0211040c3a09578cb648e45640561dd
Bryce Hull <bahull@live.com>
2018-06-03 16:28:15 -0500

    Updated with new kata

--- index.js
+++ index.js
@@ -738,3 +738,76 @@ const birdCode = arr => {
   });
   return resultsArray;
 };
+
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+// It is much easier to understand with an example:
+
+// formatDuration(62)    // returns "1 minute and 2 seconds"
+// formatDuration(3662)  // returns "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// Note that spaces are important.
+
+// Detailed rules
+// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+
+function formatDuration(time) {
+  if (time === 0) return "now";
+
+  let years = Math.floor(time / 31536000);
+  let days = Math.floor((time - years * 31536000) / 86400);
+  let hours = Math.floor((time - (years * 31536000 + days * 86400)) / 3600);
+  let minutes = Math.floor(
+    (time - (years * 31536000 + days * 86400 + hours * 3600)) / 60
+  );
+  let seconds =
+    time - (years * 31536000 + days * 86400 + hours * 3600 + minutes * 60);
+
+  let resultExecution = {
+    year: years,
+    day: days,
+    hour: hours,
+    minute: minutes,
+    second: seconds
+  };
+  let resultsArray = [];
+  for (prop in resultExecution) {
+    if (resultExecution[prop] == false) {
+      if (resultExecution[prop] > 1) {
+        resultsArray.push(`${resultExecution[prop]} ${prop}s`);
+      } else {
+        resultsArray.push(`${resultExecution[prop]} ${prop}`);
+      }
+    }
+  }
+  if (resultsArray.length === 5) {
+    return `${resultsArray[0]}, ${resultsArray[1]}, ${resultsArray[2]}, ${
+      resultsArray[3]
+    } and ${resultsArray[4]}`;
+  } else if (resultsArray.length === 4) {
+    return `${resultsArray[0]}, ${resultsArray[1]}, ${resultsArray[2]} and ${
+      resultsArray[3]
+    }`;
+  } else if (resultsArray.length === 3) {
+    return `${resultsArray[0]}, ${resultsArray[1]} and ${resultsArray[2]}`;
+  } else if (resultsArray.length === 2) {
+    return `${resultsArray[0]} and ${resultsArray[1]}`;
+  } else {
+    return `${resultsArray[0]}`;
+  }
+}
+
+formatDuration(316);
