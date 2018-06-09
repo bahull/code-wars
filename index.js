63b2db0723e0d413017dacbf858286d22a30d444
Bryce Hull <bahull@live.com>
2018-06-09 17:00:39 -0500

    Updated with new problem

--- index.js
+++ index.js
@@ -834,3 +834,22 @@ var wireCode = (function() {
 })(global);
 
 Bomb.CutTheWire(wireCode);
+
+
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string){
+  let replace5 = string.replace(/5/g, "S")
+  let replace0 = replace5.replace(/0/g, "O")
+  let replace1 = replace0.replace(/1/g, "I")
+  return replace1
+  }
