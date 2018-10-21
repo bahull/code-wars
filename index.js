4408ca25ecefc9654f0a05199769475508562de0
Bryce Hull <bahull@live.com>
2018-10-20 20:51:12 -0500

    Updated with new problem

--- index.js
+++ index.js
@@ -853,3 +853,16 @@ function correct(string){
   let replace1 = replace0.replace(/1/g, "I")
   return replace1
   }
+
+//   Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Examples:
+
+// hello "john"   => "Hello, John!"
+// hello "aliCE"  => "Hello, Alice!"
+// hello          => "Hello, World!" # name not given
+// hello ''       => "Hello, World!" # name is an empty String
+
+const hello = name => `Hello, ${name ? name.split("")[0].toUpperCase() + name.split("").slice(1).join("").toLowerCase() : "World"}!`
